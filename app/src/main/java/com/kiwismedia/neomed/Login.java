010aa076ea1f0a52fb9e9a9f5396e236e49c0038
Anshu Kumar Shandilya <ashandilya64@gmail.com>
2020-06-12 00:07:39 +0530

    Update

--- app/src/main/java/com/kiwismedia/neomed/Login.java
+++ app/src/main/java/com/kiwismedia/neomed/Login.java
@@ -6,12 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.card.MaterialCardView;
+import com.kiwismedia.neomed.ui.home.HomeFragment;
+
 public class Login extends AppCompatActivity {
+
+    MaterialCardView materialCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        materialCardView = findViewById(R.id.login_Btn);
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iLogin = new Intent(Login.this, MainActivity.class);
+                startActivity(iLogin);
+                finish();
+            }
+        });
 
     }
 
